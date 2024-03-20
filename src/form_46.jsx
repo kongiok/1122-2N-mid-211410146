284d1cfddf4e72de94b760d172fa72731effcb37
kuang-yu-wang <211410146@o365.tku.edu.tw>
2024-03-20 21:31:09 +0800

    Add feature task add

--- src/form_46.jsx
+++ src/form_46.jsx
@@ -1,10 +1,38 @@
 import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { supabase } from './utils/db';
+import { toast } from 'react-toastify';
 
 const Form_46 = () => {
   const [newItemName, setNewItemName] = useState('');
-
+  const {mutate: createTask, isLoading }= useMutation({
+    mutationFn: async (newItemName) => {
+      const { data, error } = await supabase.from('task_46').insert([
+        { title: newItemName, isdone: false },
+      ]).select();
+      if (error) {
+        throw new Error(error);
+      }
+      return data;
+    },
+    onSuccess: ()=> {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      setNewItemName('');
+    }
+  });
+  const queryClient = useQueryClient();
+  
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(newItemName.trim() === ''){
+      toast.warning('Task name cannot be empty');
+      return;
+    }
+    createTask(newItemName);
+    toast.success(`Added task: ${newItemName}`);
+    return;
   };
   return (
     <form onSubmit={handleSubmit}>
