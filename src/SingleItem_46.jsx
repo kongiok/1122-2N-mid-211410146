05feee8651d08e1389cad27d97f2c83b3e688236
kongiok <211410146@o365.tku.edu.tw>
2024-03-27 20:06:53 +0800

    Fix single item file

--- src/SingleItem_46.jsx
+++ src/SingleItem_46.jsx
@@ -1,15 +1,57 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { supabase } from './utils/db';
+import { toast } from 'react-toastify';
+
 const SingleItem_46 = ({ item }) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: async ({ taskId, isDone }) => {
+      try {
+        const { data, error } = await supabase
+          .from('task_46')
+          .update({ is_done: isDone })
+          .eq('id', taskId)
+          .select();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        query: ['task'],
+      });
+      toast.success(`Task Updated!`);
+    },
+  });
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: async (taskId) => {
+      try {
+        const { error } = await supabase
+          .from('task_46')
+          .delete()
+          .eq('id', taskId);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('task deleted');
+    },
+  });
   return (
     <div className='single-item'>
       <input
         type='checkbox'
-        checked={item.isdone}
-        onChange={() => console.log('edit task')}
+        checked={item.isDone}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.is_done })}
       />
       <p
         style={{
           textTransform: 'capitalize',
-          textDecoration: item.isdone && 'line-through',
+          textDecoration: item.isDone && 'line-through',
         }}
       >
         {item.title}
@@ -17,7 +59,7 @@ const SingleItem_46 = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
